66dee5b56f44ea7a93cee26e128fcc53fa70bde3
ranierig <ranierig@gmail.com>
2025-02-02 09:23:14 -0300

    podcast

---
+++ DesafioPodcast/DesafioPodcast/Episodio.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+
+class Episodio {
+    public Episodio(int duracaoEpisodio, int ordemEpisodio, string tituloEpisodio) {
+        DuracaoEpisodio = duracaoEpisodio;
+        OrdemEpisodio = ordemEpisodio;
+        TituloEpisodio = tituloEpisodio;
+    }
+    public List<string> ListaConvidados { get; set; } = new List<string>();
+    public int DuracaoEpisodio { get; }
+    public int OrdemEpisodio { get; }
+    public string TituloEpisodio { get; }
+    public string Resumo => $"Episódio -> Ordem: {OrdemEpisodio} | Título: {TituloEpisodio} | Duração: {DuracaoEpisodio} | Convidados: {string.Join(", ",ListaConvidados.OrderBy(c =>c))}";
+
+    public void AdicionarConvidados (string nomeConvidado) {
+        ListaConvidados.Add (nomeConvidado);
+    }
+}
