88d864e401db24f4e1be781c624d8bb8e88eb538
ranierig <ranierig@gmail.com>
2025-03-01 18:27:51 -0300

    Linqs

---
+++ AppMusic/AppMusic/Filters/FLinqFilter.cs
@@ -0,0 +1,21 @@
+﻿using AppMusic.Model;
+
+namespace AppMusic.Filters; 
+internal class FLinqFilter {
+    public static void ShowGenres (List<MListMusics> mList) {
+        var genres = mList.Select(genre => genre.Genre).Distinct().ToList();
+        genres.ForEach(genres =>  Console.WriteLine(genres));
+    }
+    public static void ShowOrderByArtists(List<MListMusics> mList) {
+        var listArtists = mList.OrderBy(artist => artist.Artist).Select(artist => artist.Artist).Distinct().ToList();
+        listArtists.ForEach(artist => Console.WriteLine(artist)) ;
+    }
+    public static void ShowArtistsGenre (List<MListMusics> mList, string genre) {
+        var listArtists = mList.Where(artist => artist.Genre.ToUpper().Contains(genre.ToUpper())).Select(artist => artist.Artist).ToList();
+        listArtists.ForEach(artists => Console.WriteLine(artists));
+    }
+    public static void ShowMusicsArtists (List<MListMusics> mList, string artist) {
+        var listMusics = mList.Where(music => music.Artist.ToUpper().Contains(artist.ToUpper())).Select(music => music.Music).ToList();
+        listMusics.ForEach(music => Console.WriteLine(music));
+    }
+}
--- AppMusic/AppMusic/Program.cs
+++ AppMusic/AppMusic/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System.Text.Json;
+using AppMusic.Filters;
 using AppMusic.Model;
+
 using (HttpClient httpClient = new() ) {
     try {
         string responseList = await httpClient.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musics = JsonSerializer.Deserialize<List<MListMusics>>(responseList)!;
         //Console.WriteLine(responseList);
-        musics[0].ShowMusics();
+        //musics[0].ShowMusics();
+        //FLinqFilter.ShowGenres(musics);
+        //FLinqFilter.ShowOrderByArtists(musics);
+        //FLinqFilter.ShowArtistsGenre(musics,"Rock");
+        FLinqFilter.ShowMusicsArtists(musics, "U2");
     } catch (Exception ex) {
-        Console.WriteLine("Erro ao chamar a API de listas de músicas.");    
+        Console.WriteLine($"Erro ao chamar a API de listas de músicas: {ex}");    
     }
 
 
